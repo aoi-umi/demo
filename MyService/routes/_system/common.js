ab9541653b5ae8961f880e02ec6406c5bc8d7763
aoi-umi <umi aoi>
2017-08-22 23:26:25 +0800

    增加soap

--- MyService/routes/_system/common.js
+++ MyService/routes/_system/common.js
@@ -530,3 +530,16 @@ exports.logSave = function(log) {
         log.res = JSON.stringify(log.res);
     return logService.save(log);
 };
+
+// var soap = require('soap');
+// var url = 'http://www.webxml.com.cn/WebServices/WeatherWebService.asmx?wsdl';
+// var args = { byProvinceName: '浙江'};
+// soap.createClient(url, function(err, client) {
+//     client.getSupportCity(args, function(err, result) {
+//         if (err) {
+//             console.log(err);
+//         }else {
+//             console.log(result);
+//         }
+//     });
+// });
