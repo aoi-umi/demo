7c097cd1d84412359608b6ba856266b7d22e0975
aoi-umi <1641622365@qq.com>
2017-10-25 16:36:34 +0800

    mainContentType增加查询条件

---
+++ MyService/public/js/dev/my.vaild.js
@@ -0,0 +1,41 @@
+/**
+ * Created by bang on 2017-9-21.
+ */
+namespace('my.vaild');
+my.vaild = {
+    isInt: function(val, type){
+        //- 0 +
+        //1 1 1 -> 0-7
+        var reg = /^-?[\d]+$/;
+        var t = '';
+        if(typeof type == 'number'){
+            while(type > 0){
+                t = type % 2 + t;
+                type = parseInt(type / 2);
+            }
+        }else{
+            t = type;
+        }
+        switch (t) {
+            case '001':
+                reg = /[1-9][\d]*/;
+                break;
+            case '010':
+                reg = /0+/;
+                break;
+            case '011':
+                reg = /^[\d]+$/;
+                break;
+            case '100':
+                reg = /^-[\d]+$/;
+                break;
+            case '101':
+                reg = /^-?[1-9][\d]*$/;
+                break;
+            case '110':
+                reg = /^-[\d]+$/;
+                break;
+        }
+        return reg.test(val);
+    }
+};
