1f54480e304d2e454b10cfd318f59fcccf742c43
aoi-umi <1641622365@qq.com>
2017-11-07 17:22:04 +0800

    获取时间差

--- MyService/routes/socket.js
+++ MyService/routes/socket.js
@@ -2,39 +2,39 @@
  * Created by umi on 2017-8-31.
  */
 var common = require('./_system/common');
-var mySocket = module.exports = {
-    onlineCount: 0,
-    io: null,
-    init: function (io) {
-        mySocket.io = io;
-        mySocket.bindEvent();
-    },
-    bindEvent: function () {
-        var io = mySocket.io;
-        io.on('connection', function (socket) {
-            mySocket.onlineCount++;
-            io.sockets.emit('onlineCount', mySocket.onlineCount);
+var mySocket = exports;
 
-            socket.on('postMsg', function(opt) {
-                //将消息发送到除自己外的所有用户
-                socket.broadcast.emit('newMsg', {
-                    datetime: common.dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss'),
-                    user: opt.user,
-                    content: opt.content,
-                    msgId: opt.msgId,
-                });
-                socket.emit('postSuccess', {
-                    user: opt.user,
-                    msgId: opt.msgId,
-                });
+exports.onlineCount = 0;
+exports.io = null;
+exports.init = function (io) {
+    mySocket.io = io;
+    mySocket.bindEvent();
+};
+exports.bindEvent= function () {
+    var io = mySocket.io;
+    io.on('connection', function (socket) {
+        mySocket.onlineCount++;
+        io.sockets.emit('onlineCount', mySocket.onlineCount);
+
+        socket.on('postMsg', function (opt) {
+            //将消息发送到除自己外的所有用户
+            socket.broadcast.emit('newMsg', {
+                datetime: common.dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+                user: opt.user,
+                content: opt.content,
+                msgId: opt.msgId,
             });
-
-            socket.on('disconnect', function () {
-                if (mySocket.onlineCount > 0) {
-                    mySocket.onlineCount--;
-                    io.sockets.emit('onlineCount', mySocket.onlineCount);
-                }
+            socket.emit('postSuccess', {
+                user: opt.user,
+                msgId: opt.msgId,
             });
         });
-    }
+
+        socket.on('disconnect', function () {
+            if (mySocket.onlineCount > 0) {
+                mySocket.onlineCount--;
+                io.sockets.emit('onlineCount', mySocket.onlineCount);
+            }
+        });
+    });
 };
