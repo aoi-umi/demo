09fe030cb193d46ad6bfc1ac182f5d528a745971
aoi-umi <1641622365@qq.com>
2017-11-10 18:56:33 +0800

    mainContent修改status

---
+++ MyService/routes/module/mainContent.js
@@ -0,0 +1,14 @@
+/**
+ * Created by bang on 2017-9-7.
+ */
+var common = require('../_system/common');
+var myEnum = require('./../_system/enum');
+var autoBll = require('../_bll/auto');
+
+exports.detailQuery = function(opt, viewOpt){
+    return autoBll.custom('main_content', 'detailQuery', opt).then(function (t) {
+        if(viewOpt){
+            viewOpt.mainContentDetail = t;
+        }
+    });
+}
