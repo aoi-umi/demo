f97480a66bc10f3a7fcc71717203c0422726ce85
aoi-umi <1641622365@qq.com>
2017-12-01 15:46:30 +0800

    添加用户信息修改日志

--- MyService/routes/_bll/user_info.js
+++ MyService/routes/_bll/user_info.js
@@ -18,25 +18,50 @@ exports.isAccountExist = function (account) {
 exports.save = function (opt) {
     var user = opt.user;
     var now = common.dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    var userInfoLog = {
+        user_info_id: user.id,
+        type: 0,
+        content: '',
+        create_date: now,
+    };
     return common.promise().then(function () {
-        if (opt.newPassword) {
-            if (!opt.password)
-                throw common.error('原密码不能为空', 'ARGS_ERROR');
-            return autoBll.detailQuery('user_info', {id: user.id}).then(function (t) {
-                if (!t || !t.id)
-                    throw common.error('查询用户信息为空');
+        return autoBll.detailQuery('user_info', {id: user.id}).then(function (t) {
+            if (!t || !t.id)
+                throw common.error('查询用户信息为空');
+            var isChanged = false;
+            if (opt.newPassword && opt.newPassword != t.password) {
+                if (!opt.password)
+                    throw common.error('原密码不能为空', 'ARGS_ERROR');
                 if (t.password != opt.password)
                     throw common.error('密码不正确');
-            });
-        }
+                isChanged = true;
+                userInfoLog.content += '[修改了密码]';
+            } else {
+                opt.newPassword = null;
+            }
+            if (!opt.nickname || opt.nickname == t.nickname)
+                opt.nickname = null;
+            else {
+                isChanged = true;
+                userInfoLog.content += `[修改了昵称: ${t.nickname} -> ${opt.nickname} ]`;
+            }
+
+            if (!isChanged) {
+                throw common.error('没有变更的信息');
+            }
+        });
     }).then(function () {
         var saveOpt = {
             id: user.id,
-            nickname: opt.nickname || null,
-            password: opt.newPassword || null,
+            nickname: opt.nickname,
+            password: opt.newPassword,
             edit_datetime: now,
         };
-        return autoBll.save('user_info', saveOpt);
+        return autoBll.tran(function (conn) {
+            return autoBll.save('user_info', saveOpt, conn).then(function () {
+                return autoBll.save('user_info_log', userInfoLog, conn);
+            });
+        });
     }).then(function () {
         if (user.key) {
             if (opt.newPassword)
@@ -47,5 +72,15 @@ exports.save = function (opt) {
                 return cache.set(user.key, user, seconds);
             }
         }
+    });
+};
+
+exports.detailQuery = function (opt) {
+    return autoBll.customDal('user_info', 'detailQuery', {id: opt.id}).then(function (t) {
+        var detail = {
+            user_info: t[0][0],
+            user_info_log: t[1],
+        }
+        return detail;
     });
 };
