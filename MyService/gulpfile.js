2cc78a8e13c6b74a10dbbe6be3fe431e4a46cb20
umi-aoi <1641622365@qq.com>
2018-02-14 14:19:44 +0800

    gulp调整

--- MyService/gulpfile.js
+++ MyService/gulpfile.js
@@ -1,37 +1,38 @@
 var gulp = require('gulp'),
-	del = require('del'), //删除文件
-	replace = require('gulp-replace-path'), //替换文件内容
-	babel = require("gulp-babel");
+    del = require('del'), //删除文件
+    replace = require('gulp-replace-path'), //替换文件内容
+    babel = require('gulp-babel'),
+    gulpSequence = require('gulp-sequence');
 
-gulp.task('clear-template', function() {
-	return del([
-		'views/_template_web/*'
-	]);
+gulp.task('clear-template', function () {
+    return del([
+        'views/_template_web/*'
+    ]);
 });
 
-gulp.task('make-template', function() {
-	gulp.src('views/_template/**')
-		.pipe(replace('<%', '{%'))
-		.pipe(replace('%>', '%}'))
-		.pipe(gulp.dest('views/_template_web'));
+gulp.task('make-template', function () {
+    gulp.src('views/_template/**')
+        .pipe(replace('<%', '{%'))
+        .pipe(replace('%>', '%}'))
+        .pipe(gulp.dest('views/_template_web'));
 });
 
-gulp.task('template', ['clear-template', 'make-template']);
+gulp.task('template', gulpSequence('clear-template', 'make-template'));
 
-gulp.task('clear-web-js', function() {
-	return del([
-		'public/js/prd/*'
-	]);
+gulp.task('clear-web-js', function () {
+    return del([
+        'public/js/prd/*'
+    ]);
 });
 
-gulp.task('make-web-js', function() {
-	gulp.src("public/js/dev/**")
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest("public/js/prd/"));
+gulp.task('make-web-js', function () {
+    gulp.src('public/js/dev/**')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('public/js/prd/'));
 });
 
-gulp.task('web-js', ['clear-web-js', 'make-web-js']);
+gulp.task('web-js', gulpSequence('clear-web-js', 'make-web-js'));
 
 gulp.task('default', ['template', 'web-js'])
