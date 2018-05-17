71d928424904044abd8795c4571b0d403c38e575
aoi-umi <1641622365@qq.com>
2018-05-17 16:01:13 +0800

    修复一些bug

---
+++ MyService/typings/libs.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="jquery" />
+
+interface JQueryStatic {
+	ui: any;
+	cookie: any;
+}
+
+interface JQuery {
+	autocomplete: any;
+}
+
+interface File {
+	mozSlice: any;
+	webkitSlice: any;
+}
+
+
+declare const SparkMD5: SparkMD5;
+interface SparkMD5 {
+	hash: any;
+	ArrayBuffer: any;
+}
+
+declare const ejs: any;
