b13918b4adb58b77536240a33bcb5aec58126ade
pc-xl <1641622365@qq.com>
2018-07-10 15:13:52 +0800

    help页

--- MyService/routes/viewBll/help.ts
+++ MyService/routes/viewBll/help.ts
@@ -2,27 +2,12 @@ import * as common from '../_system/common';
 import * as main from '../_main';
 export let get = function (opt, viewOpt) {
     var restList = [];
-    // var restConfig = main.routeConfig;
-    // for (var i = 0; i < restConfig.length; i++) {
-    //     var rest: any = restConfig[i];
-    //     var isRouter = true;
-    //     if (!rest.method)
-    //         rest.method = 'post';
-    //     switch (rest.method.toLowerCase()) {
-    //         case 'get':
-    //         case 'post':
-    //             break;
-    //         default:
-    //             isRouter = false;
-    //             break;
-    //     }
-    //     if (isRouter) {
-    //         var exampleRequest = rest.exampleRequest;
-    //         if (typeof exampleRequest == 'object')
-    //             exampleRequest = JSON.stringify(exampleRequest);
-    //         restList.push({ url: rest.url, method: rest.method, exampleRequest: exampleRequest });
-    //     }
-    // }
+    var urlConfigs = main.accessableUrlConfig;
+    for (var i = 0; i < urlConfigs.length; i++) {
+        var urlConfig = urlConfigs[i];
+        let method = /^\/interface\//.test(urlConfig.url) ? 'post' : 'get';
+        restList.push({ url: urlConfig.url, method: method, });
+    }
     viewOpt.title = 'help';
     viewOpt.restList = restList;
 };
