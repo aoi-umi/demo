9150a672dd01c0746d354a1220c8d8ab3c0198aa
pc-xl <1641622365@qq.com>
2018-09-03 12:26:51 +0800

    fix

--- test/js/db.ts
+++ test/js/db.ts
@@ -5,6 +5,7 @@ import * as  data_1 from 'typegoose/lib/data';
 import { Omit, RecursivePartial } from 'sequelize-typescript/lib/utils/types';
 Typegoose.prototype['setModelForClass'] = function (t, { existingMongoose, schemaOptions, existingConnection } = {}) {
     const name = this.constructor.name;
+    //=======
     //多个模型继承同一parent, 会覆盖
     // // get schema of current model
     // let sch = this.buildSchema(name, schemaOptions);
@@ -17,6 +18,7 @@ Typegoose.prototype['setModelForClass'] = function (t, { existingMongoose, schem
     //     // next parent
     //     parentCtor = Object.getPrototypeOf(parentCtor.prototype).constructor;
     // }
+    //=======
     let className = [];
     let parentCtor = Object.getPrototypeOf(this.constructor.prototype).constructor
     while (parentCtor && parentCtor.name !== 'Typegoose' && parentCtor.name !== 'Object') {
@@ -48,6 +50,7 @@ Typegoose.prototype['setModelForClass'] = function (t, { existingMongoose, schem
     });
 
     let sch = this.buildSchema(name, schemaOptions);
+    //=======
 
     let model = mongoose.model.bind(mongoose);
     if (existingConnection) {
@@ -62,7 +65,7 @@ Typegoose.prototype['setModelForClass'] = function (t, { existingMongoose, schem
 }
 
 type DefaultInstance = mongoose.Document & Typegoose;
-declare type ModelType<T, TT> = mongoose.Model<InstanceType<T>> & TT;
+export declare type ModelType<T, TT> = mongoose.Model<InstanceType<T>> & TT;
 
 export type FilteredModelAttributes<T extends DefaultInstance> =
     RecursivePartial<Omit<T, keyof DefaultInstance>> & {
@@ -80,38 +83,7 @@ declare module "mongoose" {
 export class Model<T> extends Typegoose {
 }
 
-class A extends Model<A>{
-    @prop({
-        required: false,
-    })
-    name?: string;
-    get num() {
-        return 1;
-    }
-
-    @staticMethod
-    static staticMethod(this: ModelType<A, typeof A>) {
-        console.log('staticMethod');
-    }
-
-    @instanceMethod
-    instanceMethod(this: InstanceType<A>) {
-        console.log('instanceMethod');
-    }
-}
-
 export function getModelForClass<T extends Typegoose, TT>(t: { new(): T }, getModelForClassOptions?: GetModelForClassOptions) {
     const model = new t().getModelForClass(t, getModelForClassOptions) as any as Typegoose & mongoose.Model<InstanceType<T>> & TT;
     return model;
 }
-
-const AModel = getModelForClass<A, typeof A>(A, {
-    schemaOptions: {
-        collection: 'userTest1'
-    }
-});
-AModel.staticMethod();
-let aModel = new AModel();
-aModel.instanceMethod()
-
-
