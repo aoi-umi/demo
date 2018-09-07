9310542d5b13e0de4f64583d157169e395d77124
pc-xl <1641622365@qq.com>
2018-09-07 14:21:14 +0800

    fix bug

--- MyService/routes/bll/log.ts
+++ MyService/routes/bll/log.ts
@@ -5,16 +5,13 @@ import { Op, WhereOptions } from 'sequelize';
 import * as autoBll from './_auto';
 import * as common from '../_system/common';
 import { LogModel } from '../dal/models/dbModel';
-import { replaceSpCharLike } from '../_system/db';
 export let query = function (opt) {
     return common.promise(async () => {
-        let options = autoBll.createQueryOption(LogModel.Log, opt);
-        let where = options.where as WhereOptions<LogModel.Log>;
-        //like option
-        ['url', 'application', 'method', 'methodName', 'req', 'res', 'ip', 'remark', 'requestIp'].forEach(key => {            
-            if (where[key])
-                where[key] = { [Op.like]: `%${replaceSpCharLike(options.where[key])}%` };
+        let options = autoBll.createQueryOption(LogModel.Log, opt, {
+            //like option
+            likeKeyList: ['url', 'application', 'method', 'methodName', 'req', 'res', 'ip', 'remark', 'requestIp']
         });
+        let where = options.where as WhereOptions<LogModel.Log>;        
         if (opt.createDateStart || opt.createDateEnd) {
             where.createDate = {};
             if (opt.createDateStart)
