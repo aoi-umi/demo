d9649e468aa751f3de347c1cc81fa86ef01fcbc2
pc-xl <1641622365@qq.com>
2018-09-25 11:36:36 +0800

    fix bug

--- MyService/src/config.ts
+++ MyService/src/config.ts
@@ -1,14 +1,15 @@
 /**
  * Created by umi on 2017-5-29.
  */
+import * as path from 'path';
 export default {
     name: 'MyService',
     port: 3010,
     deploy: '20180531',
     version: '0.0.2',
     env: 'dev',//ides 演示练习、dev 开发、qas 质量保证、prd 生产系统
-    errorDir: __dirname + '/error',
-    fileDir: __dirname + '/file',
+    errorDir: path.resolve(__dirname + '/../file/error'),
+    fileDir: path.resolve(__dirname + '/../file/upload'),
     cachePrefix: 'myService',
     redis: {
         host: '127.0.0.1',
