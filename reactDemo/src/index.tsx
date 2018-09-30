fc09116ef62b1dd787617d090153f558548d99d2
pc-xl <1641622365@qq.com>
2018-09-30 12:23:39 +0800

    redux -> mobx

--- reactDemo/src/index.tsx
+++ reactDemo/src/index.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Store, createStore } from 'redux';
+import { configure } from 'mobx';
+import { Provider } from 'mobx-react';
+configure({ enforceActions: 'always' });
 
 import './styles.css';
-import rootReducer from './modules/main/reducer';
 import App from './modules/main/components/App';
 
 document.title = 'Title';
-const initialState = {};
-const store: Store<any> = createStore(rootReducer, initialState);
 render((
-    <Provider store={store}>
+    <Provider store={{}}>
         <App />
     </Provider>
 ), document.getElementById('main'));
--- reactDemo/src/modules/test/model.ts
+++ reactDemo/src/modules/test/model.ts
@@ -1,6 +1,31 @@
-export type Test = {
-    text: string;
-    count: number;
-};
 
-export type IState = Test;
+import { observable, action, runInAction } from 'mobx';
+export class Test {
+    @observable
+    text: string;
+
+    @observable
+    count: number;
+    constructor() {
+        this.init();
+    }
+    @action
+    init = () => {
+        this.text = '';
+        this.count = 0;
+    }
+
+    @action
+    setCount = (val: number) => {
+        this.count = val;
+    };
+
+    @action
+    addCount = () => {
+        this.count++;
+    };
+    @action
+    setText = (text?: string) => {
+        this.text = text || 'world';
+    };
+}
