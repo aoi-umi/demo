56661d1ef70655ab5af76ee33f8b4a4a678551f0
pc-xl <1641622365@qq.com>
2018-11-05 15:58:17 +0800

    添加dialog

---
+++ reactDemo/src/components/MyDialog/MyDialog.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import { observer } from 'mobx-react';
+
+import { MyDialogModel } from "./model";
+
+export type MyDialogButtonType = {
+    text: string;
+    type?: string;
+}
+type MyDialogProps = {
+    title?: string;
+    children: React.ReactNode;
+    onClose?: (event, type) => void,
+    btnList?: MyDialogButtonType[]
+};
+
+@observer
+export default class MyDialog extends React.Component<MyDialogProps> {
+    private model = new MyDialogModel();
+    private btnList: MyDialogButtonType[];
+    constructor(props) {
+        super(props);
+        this.btnList = this.props.btnList || [{ text: '确认' }];
+
+    }
+    private handleClose = (event, type?: string) => {
+        this.model.toggle(false);
+        this.props.onClose && this.props.onClose(event, type);
+    }
+    render() {
+        return (
+            <Dialog
+                open={this.model.open}
+                onClose={this.handleClose}
+                scroll={'paper'}
+                aria-labelledby="scroll-dialog-title"
+                fullWidth={true}
+            >
+                <DialogTitle id="scroll-dialog-title">{this.props.title || '提示'}</DialogTitle>
+                <DialogContent>
+                    {this.props.children}
+                </DialogContent>
+                <DialogActions>
+                    {this.btnList.map((ele, idx) => {
+                        return (
+                            <Button key={idx} onClick={(event) => {
+                                this.handleClose(event, ele.type);
+                            }} color="primary">
+                                {ele.text}
+                            </Button>
+                        );
+                    })}
+                </DialogActions>
+            </Dialog>
+        );
+    }
+}
