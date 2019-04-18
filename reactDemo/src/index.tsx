dcb0dfa0c55be29bce9a99f58a59ff4ae30ccd08
pc-xl <1641622365@qq.com>
2019-04-18 18:40:01 +0800

    调整

--- reactDemo/src/index.tsx
+++ reactDemo/src/index.tsx
@@ -3,14 +3,12 @@ import { render } from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import { configure } from 'mobx';
 import { Provider } from 'mobx-react';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import { TextTransformProperty } from 'csstype';
 
 import './styles.css';
 import config from './config/env';
 import App from './modules/main/App';
 import { Test } from './modules/test/model';
+import { withCustomTheme } from './helpers';
 
 // configure({ enforceActions: 'always' });
 document.title = config.title;
@@ -18,25 +16,6 @@ const storage = {
     test: new Test()
 }
 
-const customTheme = {
-    typography: {
-        useNextVariants: true,
-    },
-    overrides: {
-        MuiButton: {
-            root: {
-                textTransform: 'none' as TextTransformProperty
-            }
-        }
-    }
-};
-export function withCustomTheme(child) {
-    return (
-        <MuiThemeProvider theme={createMuiTheme(customTheme)}>
-            {child}
-        </MuiThemeProvider>
-    );
-}
 render((
     <Provider {...storage}>
         <Router>
