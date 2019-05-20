ea29e038f939d9f6b03f1f684a7943a6d872c3c6
pc-xl <1641622365@qq.com>
2019-05-20 18:18:01 +0800

    menu

--- vueDemo/src/utils/index.ts
+++ vueDemo/src/utils/index.ts
@@ -1,7 +1,10 @@
-export function convertToClass<typeofT>(t) {
+export function convertToClass<typeofT, T = {}>(t) {
     return t as {
-        new(props: Partial<typeofT> & {
+        new(props: Partial<typeofT & T> & {
             onClick?: () => any
+            ref?: any;
+            class?: any;
+            style?: { [key: string]: any };
         }): any
     };
 }
