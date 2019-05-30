be37c238af0c481a6575d8f18dac3047d24c2eea
aoi-umi <1641622365@qq.com>
2019-05-30 23:18:11 +0800

    登录按钮

--- vueDemo/src/main.ts
+++ vueDemo/src/main.ts
@@ -5,9 +5,10 @@ Vue.use(iView);
 import App from './App';
 import router from './router';
 import store from './store';
+import config from './config/env';
 
 Vue.config.productionTip = false;
-
+document.title = config.title;
 new Vue({
   router,
   store,
