d62f59add24efab6e39735d12ea4fd7dfa787a34
pc-xl <1641622365@qq.com>
2019-06-12 18:31:14 +0800

    danmaku

--- vueDemo/src/views/demo.tsx
+++ vueDemo/src/views/demo.tsx
@@ -1,6 +1,7 @@
 import { Component, Vue, Watch } from 'vue-property-decorator';
+import anime from 'animejs';
 
-import { Input, Card } from '@/components/iview';
+import { Input, Card, Button } from '@/components/iview';
 import { MyList } from '@/components/my-list';
 
 
@@ -12,6 +13,18 @@ export default class App extends Vue {
     public get valueLength() {
         return this.value.length;
     }
+
+
+    danmaku = '';
+    contents: {
+        idx: number,
+        msg: string,
+        animeInst?: anime.AnimeInstance,
+        finished?: boolean,
+        dom?: HTMLElement,
+        refName?: string;
+    }[] = [];
+    $refs: { board: HTMLElement; }
 
     public created() {
         console.log('created.');
@@ -41,8 +54,73 @@ export default class App extends Vue {
     protected valueWatch(newV: any, oldV: any) {
         this.msg = `new value：${newV}`;
     }
+    updated() {
+        this.updateDanmaku();
+    }
+    updateDanmaku() {
+        let width = this.$refs.board.offsetWidth;
+        let height = this.$refs.board.offsetHeight;
+        let v = 1;
+        let transXReg = /\.*translateX\((.*)px\)/i;
+        let { contents } = this;
+        contents.forEach((ele, idx) => {
+            let dom = this.$refs[ele.refName];//ele.dom;
+            if (dom && !ele.animeInst) {
+                let topLevelDict = { 0: 0 };
+                contents.forEach((cont, idx2) => {
+                    let d = this.$refs[cont.refName];//cont.dom;
+                    if (idx != idx2 && d) {
+                        let x = Math.abs(parseFloat(transXReg.exec(d.style.transform)[1]));
+                        if (!isNaN(x) && x < d.offsetWidth) {
+                            let top = d.offsetTop;
+                            if (!topLevelDict[top])
+                                topLevelDict[top] = 0;
+                            topLevelDict[top]++;
+                            let newTop = d.offsetHeight + d.offsetTop;
+                            if (newTop + dom.offsetHeight >= height)
+                                newTop = 0;
+                            if (!topLevelDict[newTop])
+                                topLevelDict[newTop] = 0;
+                        }
+                    }
+                });
+                let s = width + dom.offsetWidth;
+                let top = 0;
+                let minLevel = -1;
+                // console.log(topLevelDict);
+                for (let key in topLevelDict) {
+                    let level = topLevelDict[key];
+                    if (minLevel < 0 || level < minLevel) {
+                        minLevel = level;
+                        top = parseFloat(key);
+                    }
+                }
+                if (top)
+                    dom.style.top = top + 'px';
+                let duration = s * 10 / v;
+                ele.animeInst = anime({
+                    targets: dom,
+                    translateX: -s,
+                    duration,
+                    easing: 'linear'
+                });
+                ele.animeInst.finished.then(() => {
+                    let content = contents[idx];
+                    if (content)
+                        content.finished = true;
+                });
+            }
+        });
+        //移除已结束
+        // for (let idx = contents.length - 1; idx >= 0; idx--) {
+        //     let ele = contents[idx];
+        //     if (ele.finished)
+        //         contents.splice(idx, 1);
+        // }
+    }
 
     protected render() {
+        let contents = this.contents;
         return (
             <div>
                 <MyList columns={[{ title: 'test', key: 'test' }]} data={this.list}></MyList>
@@ -55,6 +133,29 @@ export default class App extends Vue {
                         });
                     }
                 }}></MyList>
+
+                <div>
+                    <Input v-model={this.danmaku} />
+                    <Button on-click={() => {
+                        if (this.danmaku && this.danmaku.length) {
+                            let idx = contents.length;
+                            contents.push({ idx, msg: this.danmaku, refName: Date.now() + '' });
+                            this.danmaku = '';
+                        }
+                    }}>danmaku</Button>
+                    <Button on-click={() => { this.contents = []; }}>clear anime</Button>
+                </div>
+                <div ref='board' style={{ height: '500px', width: '500px', background: '#f7f7f7', overflow: 'hidden', position: 'relative' }}>
+                    {
+                        contents.map(ele => {
+                            let idx = ele.idx;
+                            return (
+                                <div key={idx} ref={ele.refName} style={{ display: 'inline-block', position: 'absolute', left: '100%', whiteSpace: 'nowrap' }}>
+                                    {ele.msg}
+                                </div>);
+                        })
+                    }
+                </div>
             </div>
         );
     }
