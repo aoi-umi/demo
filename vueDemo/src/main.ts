34d1ec95fd011d3d6bde507a2b7231274fc2468d
pc-xl <1641622365@qq.com>
2019-07-04 16:43:16 +0800

    富文本

--- vueDemo/src/main.ts
+++ vueDemo/src/main.ts
@@ -1,7 +1,13 @@
 import Vue from 'vue';
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
+import VueQuillEditor from 'vue-quill-editor';
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
+
 Vue.use(iView);
+Vue.use(VueQuillEditor);
 import App from './App';
 import router from './router';
 import store from './store';
