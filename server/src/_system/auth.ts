8731baaaa3d7320b6394af35c9e38615aadc11bd
umi-aoi <1641622365@qq.com>
2019-08-03 15:35:05 +0800

    路由修改权限

--- server/src/_system/auth.ts
+++ server/src/_system/auth.ts
@@ -6,70 +6,20 @@ import * as config from '../config';
 import { AuthConfigType } from '../config/authConfig';
 
 export type AuthType = AuthorityType | AuthorityType[] | AuthorityType[][];
-export type AccessUrlConfigType = {
-    url: string;
-    auth?: AuthType;
-}
 
 type AuthorityType = string | AuthConfigType;
 type UserType = {
     authority: { [key: string]: boolean }
 };
 export class Auth {
-    accessUrlConfig: AccessUrlConfigType[] = [];
     accessableIfNotExists = false;
     init(opt: {
-        accessUrlConfig: AccessUrlConfigType[],
         //accessUrlConfig中不存在时能否访问
         accessableIfNotExists?: boolean,
     }) {
         opt = common.extend({ accessableIfNoExists: false }, opt);
-        this.accessUrlConfig = opt.accessUrlConfig;
         this.accessableIfNotExists = opt.accessableIfNotExists;
     };
-
-    check(user: UserType, pathname: string) {
-        //url权限认证
-        let accessableUrl = this.getAccessableUrl(user, pathname);
-        return {
-            accessableUrl
-        };
-    }
-
-    //获取可访问的url，如传入pathname，该路径不可访问时抛出错误
-    getAccessableUrl(user: UserType, pathname?: string) {
-        let url = {};
-        let accessable = false;
-        let isUrlExist = false;
-        this.accessUrlConfig.forEach((item) => {
-            let opt = { notExistAuthority: null };
-            let result = this.isAccessable(user, item.auth, opt);
-            let isExist = item.url == pathname;
-            if (isExist) isUrlExist = true;
-            if (result) {
-                url[item.url] = true;
-                if (isExist)
-                    accessable = true;
-            } else if (isExist) {
-                let errCode = Auth.getErrorCode(opt.notExistAuthority);
-                throw common.error('', errCode);
-            }
-        });
-        if (pathname) {
-            if (!this.accessableIfNotExists) {
-                if (!isUrlExist)
-                    throw common.error('', config.error.NOT_FOUND, {
-                        format: function (msg) {
-                            return msg + ':' + pathname;
-                        }
-                    });
-            } else
-                accessable = true;
-            if (!accessable)
-                throw common.error('', config.auth.accessable.errCode);
-        }
-        return url;
-    }
 
     isAccessable(user: UserType, auth: AuthType, opt?: IsExistAuthorityOption) {
         let result = !auth
--- server/src/middleware/userAuth.ts
+++ server/src/middleware/userAuth.ts
@@ -7,46 +7,7 @@ import { auth } from '../_main';
 import { UserMapper } from "../models/mongo/user";
 
 export class UserAuthMid {
-    static normal: RequestHandler = async (req, res, next) => {
-        try {
-            req.myData = {
-                user: {
-                    _id: '',
-                    nickname: '',
-                    account: '',
-                    authority: {}
-                },
-                startTime: new Date().getTime(),
-                accessableUrl: {},
-                ip: req.realIp,
-            };
-            let userKey = req.header(config.dev.cacheKey.user);
-            if (userKey) {
-                let userKey2 = config.dev.cacheKey.user + userKey;
-                let user: Express.MyDataUser = await cache.get(userKey2);
-                if (user) {
-                    let { disableResult } = await UserMapper.accountCheck(user.account, user);
-                    if (disableResult.disabled) {
-                        user.authority = {};
-                    }
-                    req.myData.user = user;
-                } else {
-                    req.myData.user.key = userKey;
-                }
-            }
-
-            //url权限认证
-            let user = req.myData.user;
-            let pathname = req.baseUrl + req._parsedUrl.pathname;
-            let { accessableUrl } = auth.check(user, pathname);
-            req.myData.accessableUrl = accessableUrl;
-            next();
-        } catch (e) {
-            next(e);
-        }
-    }
-
-    static normalV2(authData?: AuthType) {
+    static normal(authData?: AuthType) {
         let fn: RequestHandler = async function (req, res, next) {
             try {
                 req.myData = {
