61487b91fef1084fc15bd72df11b55a854e03ca4
pc-xl <1641622365@qq.com>
2019-08-14 18:35:13 +0800

    fix file

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,16 @@ import { AddressInfo } from 'net';
 import * as config from './config';
 import * as main from './_main';
 
+import * as SocketIO from 'socket.io';
+import * as socket from './_system/socket';
+
 debug('my-application');
+
+process.on('unhandledRejection', function (e) {
+    main.logger.error('unhandledRejection');
+    main.logger.error(e);
+});
+
 
 const app = express();
 
@@ -18,44 +27,35 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static(config.fileDir));
 app.use(cors());
 
 //init
-main.init();
+main.init().then(() => {
+    main.register(app);
 
-//注册路由
-main.register(app);
+    /// catch 404 and forwarding to error handler
+    app.use(function (req, res, next) {
+        let err = new Error('Not Found');
+        err['status'] = 404;
+        res.status(404);
+        next(err);
+    });
 
-/// catch 404 and forwarding to error handler
-app.use(function (req, res, next) {
-    let err = new Error('Not Found');
-    err['status'] = 404;
-    res.status(404);
-    next(err);
+    /// error handlers
+    app.use(main.errorHandler);
+
+    app.set('port', process.env.PORT || config.env.port);
+
+    const server = app.listen(app.get('port'), '0.0.0.0', function () {
+        let address = server.address() as AddressInfo;
+        console.log([
+            '#################',
+            `# ${config.env.name} run at ${address.address}:${address.port},version:${config.env.version}`,
+            '#################',
+        ].join('\r\n'));
+    });
+    const io = SocketIO(server, { path: config.env.urlPrefix + '/socket.io' });
+    socket.init(io, main.initSocket);
 });
-
-/// error handlers
-app.use(main.errorHandler);
-
-process.on('unhandledRejection', function (e) {
-    main.logger.error('unhandledRejection');
-    main.logger.error(e);
-});
-
-app.set('port', process.env.PORT || config.env.port);
-
-const server = app.listen(app.get('port'), '0.0.0.0', function () {
-    let address = server.address() as AddressInfo;
-    console.log([
-        '#################',
-        `# ${config.env.name} run at ${address.address}:${address.port},version:${config.env.version}`,
-        '#################',
-    ].join('\r\n'));
-});
-
-import * as SocketIO from 'socket.io';
-import * as socket from './_system/socket';
-const io = SocketIO(server, { path: config.env.urlPrefix + '/socket.io' });
-socket.init(io, main.initSocket);
--- server/src/models/mongo/file/file.mapper.ts
+++ server/src/models/mongo/file/file.mapper.ts
@@ -1,53 +1,8 @@
 import { Types } from 'mongoose';
 
-import { gfs, GridFSInstance } from '../../../_system/dbMongo';
 import * as config from '../../../config';
-import { FileMetaType, File } from './file';
 
 export class FileMapper {
-    static async  upload(file: File) {
-        let rs = await gfs.upload({
-            _id: file.fileId,
-            filename: file.filename,
-            buffer: file.buffer,
-            contentType: file.contentType,
-            metadata: file.metadata
-        });
-        return rs;
-    }
-
-    static async findOne(cond: any, ifModifiedSince?: string | Date) {
-        let buf: Buffer, noModified = false;
-        let dbFile: Partial<GridFSInstance<FileMetaType>>;
-
-        function isNoModified(uploadDate: Date) {
-            return ifModifiedSince
-                && parseInt(new Date(ifModifiedSince).getTime() / 1000 as any) == parseInt(uploadDate.getTime() / 1000 as any)
-        }
-        dbFile = await gfs.findOne(cond);
-        if (dbFile) {
-            noModified = isNoModified(dbFile.uploadDate);
-            if (!noModified)
-                buf = await gfs.readFile({ _id: dbFile._id });
-        }
-
-        if (!dbFile)
-            return null;
-
-        let file = File.create({
-            ...dbFile,
-            buffer: buf,
-            fileId: dbFile._id,
-            noModified,
-        });
-        return file;
-    }
-
-    static async findById(fileId: Types.ObjectId | string, ifModifiedSince?: string | Date) {
-        fileId = new Types.ObjectId(fileId);
-        return this.findOne({ _id: fileId }, ifModifiedSince);
-    }
-
     static getImgUrl(_id, host?: string) {
         if (!_id)
             return '';
