5f4d201d18819a870d7f7a2b4f923bd5cb72ab79
pc-xl <1641622365@qq.com>
2019-10-17 16:06:43 +0800

    调整结构

--- vueDemo/src/views/content/video-detail.tsx
+++ vueDemo/src/views/content/video-detail.tsx
@@ -3,6 +3,7 @@ import moment from 'dayjs';
 
 import { testApi } from '@/api';
 import { myEnum, dev } from '@/config';
+import { convClass } from '@/components/utils';
 import { Divider, Spin } from '@/components/iview';
 import { MyLoad, IMyLoad } from '@/components/my-load';
 import { MyVideo } from '@/components/my-video';
@@ -19,11 +20,49 @@ import './video.less';
 export default class VideoDetail extends Base {
     stylePrefix = 'video-';
     $refs: { loadView: IMyLoad };
-    videoList: FileType[] = [];
 
     mounted() {
         this.$refs.loadView.loadData();
     }
+
+    render() {
+        return (
+            <MyLoad
+                ref="loadView"
+                loadFn={async () => {
+                    let query = this.$route.query;
+                    let rs = await testApi.videoDetailQuery({ _id: query._id });
+                    return rs;
+                }}
+                renderFn={(t: DetailType) => {
+                    let { detail } = t;
+                    return (
+                        <div>
+                            <VideoDetailMainView data={detail} />
+                            <Divider size='small' />
+                            <CommentView ownerId={detail._id} ownUserId={detail.userId} type={myEnum.contentType.视频} />
+                        </div>
+                    );
+                }} />
+        );
+    }
+}
+
+@Component
+class VideoDetailMain extends Base {
+    stylePrefix = 'video-';
+
+    @Prop({
+        required: true
+    })
+    data: DetailDataType;
+
+    created() {
+        this.videoList = this.data.videoList.map(ele => {
+            return { url: ele.url, fileType: FileDataType.视频, originFileType: ele.contentType };
+        });
+    }
+    videoList: FileType[] = [];
 
     renderHeader(detail: DetailDataType) {
         return (
@@ -39,42 +78,29 @@ export default class VideoDetail extends Base {
     }
 
     render() {
+        let detail = this.data;
         return (
-            <MyLoad
-                ref="loadView"
-                loadFn={async () => {
-                    let query = this.$route.query;
-                    let rs = await testApi.videoDetailQuery({ _id: query._id });
-                    this.videoList = rs.detail.videoList.map(ele => {
-                        return { url: ele.url, fileType: FileDataType.视频, originFileType: ele.contentType };
-                    });
-                    return rs;
-                }}
-                renderFn={(t: DetailType) => {
-                    let { detail } = t;
-                    return (
-                        <div>
-                            <h1>{detail.title}</h1>
-                            <br />
-                            {this.renderHeader(detail)}
-                            <br />
-                            <div class={this.getStyleName('video-box')}>
-                                <div class={this.getStyleName('video')}>
-                                    <MyVideo options={{
-                                        sources: this.videoList.map(ele => {
-                                            return {
-                                                src: ele.url,
-                                                type: ele.originFileType,
-                                            }
-                                        })
-                                    }} />
-                                </div>
-                            </div>
-                            <Divider size='small' />
-                            {detail._id && <CommentView ownerId={detail._id} ownUserId={detail.userId} type={myEnum.contentType.视频} />}
-                        </div>
-                    );
-                }} />
-        );
+            <div>
+                <h1>{detail.title}</h1>
+                <br />
+                {this.renderHeader(detail)}
+                <br />
+                <div class={this.getStyleName('video-box')}>
+                    <div class={this.getStyleName('video')}>
+                        <MyVideo options={{
+                            sources: this.videoList.map(ele => {
+                                return {
+                                    src: ele.url,
+                                    type: ele.originFileType,
+                                }
+                            }),
+                            poster: detail.coverUrl,
+                        }} />
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
+
+export const VideoDetailMainView = convClass<VideoDetailMain>(VideoDetailMain);
