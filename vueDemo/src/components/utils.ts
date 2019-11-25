db4e689c57198f4bece69055d07eec8e2d2331c6
pc-xl <1641622365@qq.com>
2019-11-25 15:41:12 +0800

    弹幕列表

--- vueDemo/src/components/utils.ts
+++ vueDemo/src/components/utils.ts
@@ -19,4 +19,39 @@ export class Utils {
     static getStyleName(stylePrefix: string, ...args: string[]) {
         return args.map(ele => stylePrefix + ele);
     }
+
+    static dateParse(date) {
+        if (typeof date == 'string')
+            date = date.replace(/-/g, '/');
+        if (!isNaN(date) && !isNaN(parseInt(date)))
+            date = parseInt(date);
+        if (!(date instanceof Date))
+            date = new Date(date);
+        return date;
+    }
+
+    static getDateDiff(date1, date2) {
+        date1 = this.dateParse(date1);
+        date2 = this.dateParse(date2);
+
+        let isMinus = false;
+        //date1 开始日期 ，date2 结束日期
+        let timestamp = date2.getTime() - date1.getTime(); //时间差的毫秒数
+        if (timestamp < 0) {
+            timestamp = -timestamp;
+            isMinus = true;
+        }
+        timestamp /= 1000;
+        let seconds = Math.floor(timestamp % 60);
+        timestamp /= 60;
+        let minutes = Math.floor(timestamp % 60);
+        timestamp /= 60;
+        let hours = Math.floor(timestamp % 24);
+        timestamp /= 24;
+        let days = Math.floor(timestamp);
+        let diff = (days ? days + ' ' : '') + ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
+        if (isMinus)
+            diff = '-' + diff;
+        return diff;
+    }
 }
